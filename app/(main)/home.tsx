8b8211ccc3ca0b834258cd47f488e8b0fcab9781
Kaveesha Archana <kaveeshaarchana2003@gmail.com>
2025-10-18 22:46:34 +0530

    Integrate socket.io-client for real-time events
    Added socket.io-client dependency and implemented socket connection management in the authentication context. Created socket and socketEvents modules to handle connection, disconnection, and event communication. Updated home screen to test socket events and changed API_URL for local development.

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -3,12 +3,26 @@ import ScreenWrapper from "@/components/ScreenWrapper"
 import Typo from "@/components/Typo"
 import { colors } from "@/constants/theme"
 import { useAuth } from "@/Contexts/authContext"
-import React from "react"
+import { testSocket } from "@/socket/socketEvents"
+import React, { useEffect } from "react"
 import { StyleSheet } from "react-native"
 
 const Home = ()=>{
     const {user, signOut} = useAuth();
     // console.log("user: ", user);
+
+    useEffect(()=>{
+        testSocket(testSocketCallbackHandler);
+        testSocket(null);
+
+        return()=>{
+            testSocket(testSocketCallbackHandler,true)
+        }
+    },[]);
+
+    const testSocketCallbackHandler = (data:any)=>{
+        console.log('got response from testSocket event: ',data)
+    }
 
     const handleLogout = async()=>{
         await signOut();
