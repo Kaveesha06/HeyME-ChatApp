41462a51a32315e61c7618d8e43a90c43a9ad821
Kaveesha Archana <kaveeshaarchana2003@gmail.com>
2025-10-19 17:54:32 +0530

    Add profile modal and avatar components
    Introduces a profile modal for updating user information and avatar, including image picker integration. Adds reusable Avatar and Header components, updates socket event for profile updates, and refactors home screen UI. Also updates navigation stack and dependencies for image picking.

---
+++ app/(main)/profileModal.tsx
@@ -0,0 +1,257 @@
+import Avatar from "@/components/Avatar";
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import Input from "@/components/Input";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/Contexts/authContext";
+import { updateProfile } from "@/socket/socketEvents";
+import { UserDataProps } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
+import * as ImagePicker from 'expo-image-picker';
+import { useRouter } from "expo-router";
+import * as Icons from "phosphor-react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Platform, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+
+
+const profileModal = () => {
+
+    const { user, signOut, updateToken } = useAuth();
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const [userData, setUserData] = useState<UserDataProps>({
+        name: "",
+        email: "",
+        avatar: null
+    })
+
+    useEffect(() => {
+        updateProfile(processUpdateProfile);
+        return()=>{
+            updateProfile(processUpdateProfile, true);
+        }
+    }, []);
+
+    const processUpdateProfile = (res:any)=>{
+        console.log('got res: ', res);
+        setLoading(false);
+
+        if(res.success){
+            updateToken(res.data.token);
+            router.back();
+        }else{
+            Alert.alert('User', res.msg);
+        }
+    
+    }
+
+    useEffect(() => {
+        setUserData({
+            name: user?.name || "",
+            email: user?.email || "",
+            avatar: user?.avatar,
+        });
+    }, [user]);
+
+    const onPickImage = async()=>{
+        let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+    //   allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setUserData({ ...userData, avatar: result.assets[0]});
+    }
+    }
+
+    const handleLogout = async ()=>{
+        router.back();
+        await signOut();
+    };
+
+    const showLogoutAlert=()=>{
+        Alert.alert("confirm", "Are you sure you want to logout?",[
+            {
+                text: "Cansel",
+                onPress:()=> console.log('cansel logout'),
+                style: 'cancel'
+            },
+            {
+                text: "Logout",
+                onPress: ()=>handleLogout(),
+                style: 'destructive'
+            }
+        ])
+    }
+    const onSubmit = () => {
+        let {name, avatar} = userData;
+        if(!name.trim()){
+            Alert.alert('User',"Please enter your name");
+            return;
+        }
+
+        let data = {
+            name,
+            avatar
+        }
+
+        setLoading(true);
+        updateProfile(data);
+    };
+
+    return (
+        <ScreenWrapper isModal={true}>
+            <View style={styles.container}>
+                <Header
+                    title={"Update Profile"}
+                    leftIcon={
+                        Platform.OS == "android" &&
+                        <BackButton color={colors.black} />
+                    }
+                    style={{ marginVertical: spacingY._15 }}
+                />
+
+                {/* form */}
+
+                <ScrollView contentContainerStyle={styles.from}>
+                    <View style={styles.avatarContainer}>
+                        <Avatar uri={userData.avatar} size={170} />
+                        <TouchableOpacity style={styles.editIcon} onPress={onPickImage} >
+                            <Icons.PencilIcon
+                                size={verticalScale(20)}
+                                color={colors.neutral800}
+                            />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={{ gap: spacingY._20 }}>
+                        <View style={styles.inputContainer}>
+                            <Typo style={{ paddingLeft: spacingX._10 }} >Email</Typo>
+                            <Input
+                                value={userData.email}
+                                containerStyle={{
+                                    borderColor: colors.neutral350,
+                                    paddingLeft: spacingX._20,
+                                    backgroundColor: colors.neutral300
+                                }}
+                                onChangeText={value =>
+                                    setUserData({ ...userData, email: value })
+                                }
+                                editable={false}
+                            />
+                        </View>
+                    </View>
+
+                    <View style={{ gap: spacingY._20 }}>
+                        <View style={styles.inputContainer}>
+                            <Typo style={{ paddingLeft: spacingX._10 }} >Name</Typo>
+                            <Input
+                                value={userData.name}
+                                containerStyle={{
+                                    borderColor: colors.neutral350,
+                                    paddingLeft: spacingX._20,
+                                    // backgroundColor: colors.neutral300
+                                }}
+                                onChangeText={value =>
+                                    setUserData({ ...userData, name: value })
+                                }
+                            // editable={false}
+                            />
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+
+            <View style={styles.footer}>
+                {
+                    !loading && (
+                        <Button
+                            style={{
+                                backgroundColor: colors.rose,
+                                height: verticalScale(56),
+                                width: verticalScale(56),
+                            }}
+                            onPress={showLogoutAlert}
+                        >
+                            <Icons.SignOutIcon
+                                size={verticalScale(30)}
+                                color={colors.white}
+                                weight="bold"
+                            />
+                        </Button>
+                    )
+                }
+
+
+                <Button style={{ flex: 1 }} onPress={onSubmit} loading={loading} >
+                    <Typo color={colors.black} fontWeight={"700"}>
+                        Update
+                    </Typo>
+                </Button>
+            </View>
+
+        </ScreenWrapper>
+    );
+};
+
+export default profileModal;
+
+const styles = StyleSheet.create({
+    editIcon: {
+        position: "absolute",
+        bottom: spacingY._5,
+        right: spacingY._7,
+        borderRadius: 100,
+        backgroundColor: colors.neutral100,
+        shadowColor: colors.black,
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.25,
+        shadowRadius: 10,
+        elevation: 4,
+        padding: spacingY._7,
+    },
+    inputContainer: {
+        gap: spacingY._7,
+    },
+    avatar: {
+        alignSelf: "center",
+        backgroundColor: colors.neutral300,
+        height: verticalScale(135),
+        width: verticalScale(135),
+        borderRadius: 200,
+        borderWidth: 1,
+        borderColor: colors.neutral500,
+    },
+    avatarContainer: {
+        position: "relative",
+        alignSelf: "center",
+    },
+    from: {
+        gap: spacingY._30,
+        marginTop: spacingY._15,
+    },
+    footer: {
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingHorizontal: spacingX._20,
+        gap: scale(12),
+        paddingTop: spacingY._15,
+        borderTopColor: colors.neutral200,
+        marginBottom: spacingY._10,
+        borderTopWidth: 2,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        paddingHorizontal: spacingY._20,
+    }
+});
